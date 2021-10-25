08d5ca75ab3b04b590502814e6f62e3d30addda7
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2021-10-26 05:00:43 +0700

    update styling

--- dummy_data.js
+++ dummy_data.js
@@ -112,6 +112,119 @@ const data = [
       <li>Mengisi formulir pendaftaran di link yang sudah disediakan</li>
     </ol>`,
   },
+  {
+    id: 4,
+    title: "Save the Ocean",
+    organization: "Marine Steward Council",
+    schedule: "5-10 Januari 2022",
+    image_url:
+      "https://images.unsplash.com/photo-1616680214084-22670de1bc82?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+    icons_img:
+      "https://cdn-icons.flaticon.com/png/512/819/premium/819814.png?token=exp=1634972149~hmac=8ee71f475d9a305b5682f63bc367f430",
+    location: "Pantai Pangandaran, Ciamis",
+    loc_url: "https://goo.gl/maps/VQQgrFb8YqV4RtKcA",
+    maxRegist: "Batas Registrasi : 3 Desember 2021",
+    decs: `            <h3>Description</h3>
+    <p>[CALL FOR VOLUNTEER]</p>
+    <p>
+        Menumbuhkan kepedulian masyarakat dalam berpartisipasi menjaga sumberdaya tersebut melalui aksi nyata, membersihkan pesisir dan laut dari sampah (plastik) dan menjadikan (menghadap) laut sebagai masa depan bangsa.
+    </p>
+    <p>
+      Bagi kamu yang tertarik dengan program ini Yuk buruan daftar dengan syarat dan ketentuan dibawah ini:
+    </p>
+    <p>
+      <a>Nama Pekerjaan : Volunteer Young Farmers Club </a> <br />
+      <a>Relawan Dibutuhkan : 120 orang </a> <br />
+      <a
+        >Tugas Relawan : Relawan diharapkan dapat membangun koneksi sekaligus menyelamatkan bumi dengan hal kecil yang dilakukan bersama-sama.</a
+      >
+    </p>
+    <a>Kriteria Relawan</a>
+    <ol type="1">
+      <li>Warga Negara Indonesia (WNI) di seluruh wilayah</li>
+      <li>Bekomitmen dan berdedikasi</li>
+      <li>Berusia diatas 15 tahun</li>
+      <li>Mengisi formulir pendaftaran di link yang sudah disediakan</li>
+    </ol>`,
+  },
+  {
+    id: 5,
+    title: "Project Tanam 1000 Pohon",
+    organization: "Greenpeace Indonesia",
+    schedule: "Minggu, 11 September 2021",
+    image_url:
+      "https://static.gatra.com/foldershared/images/2019/fatikhin/07-Jul/shutterstock_penanaman_pohon.jpg",
+    icons_img: "./assets/img/placeholder.png",
+    location: "Taman Kota Jakarta, Jakarta Pusat",
+    loc_url: "https://goo.gl/maps/CwCVZ9Yc7S6Y7aq79",
+    maxRegist: "Batas Registrasi : 9 September 2021",
+    decs: `            <h3>Description</h3>
+    <p>[CALL FOR VOLUNTEER]</p>
+    <p>
+      Program <em>#Tanampohon</em> merupakan gerakan sosial di bidang
+      lingkungan untuk memperbaiki taman kota yang ada di seluruh
+      Indonesia. Kegiatan ini akan melibatkan 500 relawan yang berjiwa
+      tulus.
+    </p>
+    <p>
+      Untuk kamu yang ingin memperbaiki bumi ini, Kuyyy Sabi banget ikut
+      program ini
+    </p>
+    <p>
+      <a>Nama Pekerjaan : Relawan Tanam Pohon </a> <br />
+      <a>Relawan Dibutuhkan : 500 orang </a> <br />
+      <a>Total Jam Kerja : 2 jam</a> <br />
+      <a
+        >Tugas Relawan : Relawan diharapkan bersedia secara sukarela
+        menanam pohon.</a
+      >
+    </p>
+    <a>Kriteria Relawan</a>
+    <ol type="1">
+      <li>Pemuda/i berusia 20-35 tahun</li>
+      <li>Berbadan sehat jasmani dan rohani</li>
+      <li>Mengikuti orientasi atau training relawan</li>
+      <li>Mengikuti aturan yang diberlakukan sesuai pedoman</li>
+    </ol>
+    <a>Perlengkapan Relawan</a><br />
+    <li>Sarung tangan</li>
+    <li>Skop (jika ada)</li>`,
+  },
+  {
+    id: 6,
+    title: "Campaign Young Farmer",
+    organization: "Dictio",
+    schedule: "4-13 Desember 2021",
+    image_url:
+      "https://media.istockphoto.com/photos/woman-take-care-of-cypress-plants-picture-id1316944376?k=20&m=1316944376&s=612x612&w=0&h=GJKMEUJmqPekO8DuwosKV4OyPhwiJlgKg4lWg8pugPk=",
+    icons_img:
+      "https://cdn-icons.flaticon.com/png/512/819/premium/819814.png?token=exp=1634972149~hmac=8ee71f475d9a305b5682f63bc367f430",
+    location: "Teras Cikapundung BBWS, Bandung",
+    loc_url: "https://goo.gl/maps/qXQwVqTdcHKwHopA8",
+    maxRegist: "Batas Registrasi : 30 November 2021",
+    decs: `            <h3>Description</h3>
+    <p>[CALL FOR VOLUNTEER]</p>
+    <p>
+      Young Farmers Club merupakan project terbaru Dictio yang mengajak milenial untuk mengisi waktu luang dengan bercocok tanam. Selain itu, sesam volunteer bisa berdiskusi seputar dunia pertanian.
+    </p>
+    <p>
+      Bagi kamu yang tertarik dengan program ini Yuk buruan daftar dengan syarat dan ketentuan dibawah ini:
+    </p>
+    <p>
+      <a>Nama Pekerjaan : Volunteer Young Farmers Club </a> <br />
+      <a>Relawan Dibutuhkan : 40 orang </a> <br />
+      <a
+        >Tugas Relawan : Relawan diharapkan dapat membangun koneksi sekaligus menyelamatkan bumi dengan hal kecil yang dilakukan bersama-sama.</a
+      >
+    </p>
+    <a>Kriteria Relawan</a>
+    <ol type="1">
+      <li>Warga Negara Indonesia (WNI) di seluruh wilayah</li>
+      <li>Memiliki akun instagram pribadi</li>
+      <li>Bekomitmen dan berdedikasi</li>
+      <li>Mengisi formulir pendaftaran di link yang sudah disediakan</li>
+    </ol>`,
+  },
 ];
 
 export default data;
